24eb296b3aa47d8276f07f645fd08fdb7184cc23
LucaRoza <lucarozanov@gmail.com>
2024-11-13 15:47:22 +0300

    global changes! SPA

---
+++ menu/main.js
@@ -0,0 +1,7 @@
+export const root = document.getElementById('root')
+
+export const sidebarMenu = document.createElement('aside')
+
+export const headMenu = document.createElement('div')
+
+export const contantMenu = document.createElement('div')
